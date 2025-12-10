b37c96385572ad3f214fab69b0ed725cb536449b
joaov17or <joao.oliveira136@outlook.com>
2025-12-10 19:59:03 +0000

    alterações no script

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "995dada41daedab2"; }
+/******/ 		__webpack_require__.h = function() { return "0dbaad28755e75cb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 function Home() {
-    return <h1> Olá, Mundo! </h1> //criando uma apresentação;
-}
+    return (
+        <div style={{ backgroundColor: "#868686ff", height: "100vh" }}>
+            <h1 style={{ fontFamily: "Arial, sans-serif" }}> Olá, Mundo! </h1> 
+        </div>
+    );
+} //criando uma apresentação;
 
 export default Home; //exportando essa função padrão para fora do arquivo;
 //export Teste2 se eu tivesse outra função
